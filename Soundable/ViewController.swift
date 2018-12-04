abbd4cb4277d3fd0b52c74b47364b4148ea00724
Luis Cardenas <lcardenas@doonamis.es>
2018-12-04 08:41:27 +0100

    Initial Commit

---
+++ Soundable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soundable
+//
+//  Created by Luis Cardenas on 04/12/2018.
+//  Copyright © 2018 ThXou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
